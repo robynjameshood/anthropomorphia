8f4d9088653365e7a69e5fd5e4c2a9f1dde0fd23
Robyn <r_blackham@outlook.com>
2022-05-23 02:32:35 +0100

    base homepage implementation

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
 <?php
+//home page
+?>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<header>
+    <h3>Anthropomorphia</h3>
+    <nav>
+        <a href="#">Account</a>
+        <a href="#">Inventory</a>
+    </nav>
+</header>
+<main class="content-boxes-wrapper">
+    <section class="content-box-one">
+        <h2>World</h2>
+        <p>Our world - explored!</p>
+    </section>
+    <section class="content-box-two">
+        <h2>Creatures</h2>
+        <p>An overview</p>
+    </section>
+    <section class="content-box-three">
+        <h2>Upcoming Content</h2>
+        <p>Developer log</p>
+    </section>
+</main>
+<article class="adventure-section">
+    <section class="background-art">
+        <p>Anthromorphia</p>
+    </section>
+</article>
+<footer class="footer-content">
+    <section class="guides">
+        <h2>Guides</h2>
+        <p>Explore Our Guides</p>
+    </section>
+    <section class="forum">
+        <h2>Forum</h2>
+        <p>Explore The Community</p>
+    </section>
+    <section class="faqs">
+        <h2>FAQ's</h2>
+        <p>Explore Our Range Of Frequently Asked Questions</p>
+    </section>
+</footer>
+</body>
